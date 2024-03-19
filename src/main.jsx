68eb62aef6b285ec603539f954f59c0c5218faf1
Destinygerald <118162858+Destinygerald@users.noreply.github.com>
2024-03-19 02:40:25 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from "react-router-dom"
+import { HashRouter } from "react-router-dom"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { Provider } from "react-redux"
 import { configureStore } from "@reduxjs/toolkit"
@@ -25,12 +25,12 @@ const store = configureStore({
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <QueryClientProvider client={client}>
         <Provider store={store}>
           <App />
         </Provider>
       </QueryClientProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
